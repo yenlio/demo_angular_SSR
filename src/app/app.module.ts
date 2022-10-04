bc4d7ad129aab4d8a5a19b978f440f1fd3d91c3d
ETC\os_yennh <yennh@etc.vn>
2022-10-04 09:37:12 +0700

    4/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './components/post-list/post-list.component';
-
+import {AngularMaterialModule} from './material.module'
+import {TransferHttpCacheModule} from '@nguniversal/common';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +14,11 @@ import { PostListComponent } from './components/post-list/post-list.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    TransferHttpCacheModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef, Input } from '@angular/core';
 import { BlogserviceService } from 'src/app/services/blogservice.service';
+import { DOCUMENT } from '@angular/common';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { finalize, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
@@ -7,10 +10,55 @@ import { BlogserviceService } from 'src/app/services/blogservice.service';
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
- postList$=this.blogservice.getPostList()
-  constructor(private readonly blogservice:BlogserviceService) { }
+  @Input()
+  requiredFileType!: string;
 
-  ngOnInit(): void {
+  fileName = '';
+  uploadProgress!: number|null;
+  uploadSub!: Subscription;
+  postList$ = this.blogservice.getPostList()
+  constructor(private readonly blogservice: BlogserviceService, @Inject(DOCUMENT) document: Document,private http: HttpClient) {
+
   }
+  @ViewChild('myDiv') table!: ElementRef
+  ngOnInit(): void {
+
+  }
+  ngAfterViewInit(): void {
+    this.table.nativeElement.style.color = "red"     
+  }
+  // onFileSelected(event:any) {
+
+  //   const file:File = event.target.files[0];
+      
+  //   if (file) {
+  //       this.fileName = file.name;
+  //       const formData = new FormData();
+  //       formData.append("thumbnail", file);
+
+  //       const upload$ = this.http.post("/api/thumbnail-upload", formData, {
+  //           reportProgress: true,
+  //           observe: 'events'
+  //       })
+  //       .pipe(
+  //           finalize(() => this.reset())
+  //       );
+      
+  //       this.uploadSub = upload$.subscribe(event => {
+  //         if (event.type == HttpEventType.UploadProgress) {
+  //           this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+  //         }
+  //       })
+  //   }
+  // }
+  // cancelUpload() {
+  //   this.uploadSub.unsubscribe();
+  //   this.reset();
+  // }
+
+  // reset() {
+  //   this.uploadProgress = null;
+  //   this.uploadSub = ;
+  // }
 
 }
